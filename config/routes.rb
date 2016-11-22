fce74a965a1635e4de7bdac2a4a5cf247ba45517
Faisal Hassan <faisal.hassanx@gmail.com>
2016-11-22 01:18:49 +0000

    Add sessions and pages controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sign_in' => 'sessions#new', as: :sign_in
+  post 'sign_in' => 'sessions#create'
+  get 'sign_out' => 'sessions#destroy', as: :sign_out
+  
   get 'sign_up' => 'users#new', as: :sign_up
-  get 'sign_up' => 'users#create'
+  post 'sign_up' => 'users#create'
 end
